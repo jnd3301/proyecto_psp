629de196e6bdd00694f04f507f9bec2ccf6e9179
Jonathan <121343480+jnd3301@users.noreply.github.com>
2023-03-05 13:08:07 +0100

    Añadida API WEB

---
+++ test/Email.cs
@@ -0,0 +1,12 @@
+﻿namespace test
+{
+    public class Email
+    {
+        public int Id { get; set; }
+        public string? Origen { get; set; }
+        public string? Destino { get; set; }
+        public string? Asunto { get; set; }
+        public string? Cuerpo { get; set; }
+
+    }
+}
---
+++ test/EmailDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace test
+{
+    class EmailDb : DbContext
+    {
+        public EmailDb(DbContextOptions<EmailDb> options)
+            : base(options) { }
+
+        public DbSet<Email> Todos => Set<Email>();
+    }
+}
